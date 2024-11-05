530ddc2522e9c5bb993ceebd48d4efa66a8c7f1b
mthalima <matheus.ecomp@gmail.com>
2024-11-05 20:52:48 -0300

    commandline runner criado retornando um hello world

--- src/main/java/com/schoolsys/schooldemo/SchooldemoApplication.java
+++ src/main/java/com/schoolsys/schooldemo/SchooldemoApplication.java
@@ -1,7 +1,9 @@
 package com.schoolsys.schooldemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SchooldemoApplication {
@@ -9,5 +11,16 @@ public class SchooldemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchooldemoApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+
+		return runner ->{
+			System.out.println("Hello World");
+		};
+	}
+
+
+
 
 }
