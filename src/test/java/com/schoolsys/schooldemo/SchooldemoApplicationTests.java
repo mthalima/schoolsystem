63b5fef95fa2f072e08ed922aac03355ce9b8c9a
mthalima <matheus.ecomp@gmail.com>
2024-11-05 20:48:07 -0300

    projeto novo

---
+++ src/test/java/com/schoolsys/schooldemo/SchooldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolsys.schooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
