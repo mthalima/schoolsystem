5bdd073f270b4d48f03386dcbfe18d210634fff6
mthalima <matheus.ecomp@gmail.com>
2024-11-19 23:29:25 -0300

    MATERIA

--- src/main/java/com/schoolsys/schooldemo/entity/Materia.java
+++ src/main/java/com/schoolsys/schooldemo/entity/Materia.java
@@ -1,4 +1,23 @@
 package com.schoolsys.schooldemo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "materia")
 public class Materia {
+
+	//ATRIBUTOS
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "nomeMateria")
+	private String nomeMateria;
+
+	@Column(name = "cargaHoraria")
+	private int cargaHoraria;
+
+	@Column(name = "detalhesMaterias")
+	private String detalhesMateria;
 }
