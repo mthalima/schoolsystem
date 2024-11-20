e6ec67485342fbaaea5b1c6aba49bacf90b39221
mthalima <matheus.ecomp@gmail.com>
2024-11-19 22:58:02 -0300

    entidade professor

--- src/main/java/com/schoolsys/schooldemo/entity/Professor.java
+++ src/main/java/com/schoolsys/schooldemo/entity/Professor.java
@@ -1,7 +1,6 @@
 package com.schoolsys.schooldemo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.List;
 
@@ -9,11 +8,24 @@ import java.util.List;
 @Table(name = "professor")
 public class Professor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
 
+	@Column(name = "nome")
 	private String nome;
 
+	@Column(name = "sobrenome")
 	private String sobrenome;
 
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "telefone")
+	private String telefone;
+
+	@Column(name = "materias")
 	private List<Materia> materias;
+
 }
